25b687ce2ed78e56149454e44319a28efd6d1788
Emily Nelson <emjeannelson@gmail.com>
2017-07-07 11:30:59 +0800

    Add about text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,6 +11,7 @@ export default class About extends React.Component {
             <p>
               This is a weather app that I built in July 2017 using React.
               This app shows the local weather when the home page loads. The user can then search for a different location. Users can also add locations from the home page to their list of saved locations. They can visit the saved locations page to see current weather conditions at all the locations they have saved.
+              The app uses the IP-API to get the user's location and the Open Weather Map API to get the weather. I chose to use the IP-API instead of HTML5 Geolocation because I wanted people to be able to view the app over HTTP instead of HTTPS, but I may make an HTTPS version in the future.
             </p>
 
             <h2 className="text-center">Process</h2>
